2c240f7e5259fb3e2186b4ef01061f5fa9f53e2d
Jordi Garcia <jordi@tokyoplay.com>
2020-10-02 14:45:05 -0500

    implementando el card del tutorial co-authored-by: NanciGarcia nancilizbethgarciamartinez@gmail.com co-authored-by: Ocosmee ronalcv93mail.com

--- recetario/components/cardItem.js
+++ recetario/components/cardItem.js
@@ -1,45 +1,50 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native'
-import { ListItem, Button } from 'react-native-elements'
+import { ListItem, Button, Icon, Card } from 'react-native-elements'
 
-const CardM = (props) => {
+const users = [
+    {
+        name: 'brynn',
+        avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+    },
+    {
+        name: '2',
+        avatar: '../images/mole.jpeg'
+    },
+    {
+        name: '3',
+        avatar: '../images/verdes.jpg'
+    },
+    {
+        name: '4',
+        avatar: '../images/discada.jpg'
+    },
+]
 
-    const users = [
-        {
-            name: 'brynn',
-            avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
-        },
-        {
-            name: '2',
-            avatar: '/images/moles.jpeg'
-        },
-        {
-            name: '3',
-            avatar: '/images/verdes.jpg'
-        },
-        {
-            name: '4',
-            avatar: '/images/discada.jpg'
-        },
-    ]
+class CardM extends React.Component {
 
-    return (
-        // implemented without image without header, using ListItem component
-        <View>
-            {
-                users.map((u, i) => {
-                    return (
-                        <ListItem
-                            key={i}
-                            roundAvatar
-                            title={u.name}
-                            leftAvatar={{ source: { uri: u.avatar } }}
-                        />
-                    );
-                })
-            }
-        </View>
-    )
+    render() {
+        return (
+            // implemented without image without header, using ListItem component
+            // implemented without image without header, using ListItem component
+            <Card containerStyle={{ padding: 0 }} >
+                {
+                    users.map((u, i) => {
+                        return (
+                            <ListItem
+                                key={i}
+                                roundAvatar
+                                title={u.name}
+                                leftAvatar={{ source: { uri: u.avatar } }}
+                            />
+                        );
+                    })
+                }
+            </Card>
+
+            
+        )
+    }
 }
 
 export default CardM;
